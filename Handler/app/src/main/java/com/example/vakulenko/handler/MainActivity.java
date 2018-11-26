7cd86ee1739a22b06c2dbfe196a7300958c6ab71
Вакуленко Алексей Владимирович <Vakulenko.A.Vla@sberbank.ru>
2018-11-26 16:32:37 +0300

    при попытку зайти на goole.com можно открыть приложение, которое бесцеремонно откроет yandex.ru

--- Handler/app/src/main/java/com/example/vakulenko/handler/MainActivity.java
+++ Handler/app/src/main/java/com/example/vakulenko/handler/MainActivity.java
@@ -3,18 +3,18 @@ package com.example.vakulenko.handler;
 import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.webkit.WebView;
 
 public class MainActivity extends Activity {
 
-    private TextView textView;
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.textView1);
-
+        webView = findViewById(R.id.web_view1);
+        webView.getSettings().setJavaScriptEnabled(true);
     }
 
     @Override
@@ -22,7 +22,7 @@ public class MainActivity extends Activity {
         super.onResume();
         Uri uri = getIntent().getData();
         if (uri != null) {
-            textView.setText(getIntent().getData().toString());
+            webView.loadUrl("https://www.yandex.ru/");
         }
     }
 
