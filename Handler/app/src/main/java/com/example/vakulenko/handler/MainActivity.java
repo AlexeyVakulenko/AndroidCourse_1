06e2bb535849b107b60177510da786e23a6840b0
Вакуленко Алексей Владимирович <Vakulenko.A.Vla@sberbank.ru>
2018-11-26 15:08:18 +0300

    onResume onPause и проверка на != null

--- Handler/app/src/main/java/com/example/vakulenko/handler/MainActivity.java
+++ Handler/app/src/main/java/com/example/vakulenko/handler/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.vakulenko.handler;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView1);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Uri uri = getIntent().getData();
+        if (uri != null) {
+            textView.setText(getIntent().getData().toString());
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 }
